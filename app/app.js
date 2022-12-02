249ef8f44b2c2ece35ed65fa1eff7040584954ea
Iván Legrán <68439095+ivanlegranbizarro@users.noreply.github.com>
2022-12-02 09:27:18 +0100

    Optimización

--- app/app.js
+++ app/app.js
@@ -1,1 +1,18 @@
-console.log('Hello World');
+import colors from "colors";
+import {inquirerMenu} from "./helpers/inquirer.js";
+
+const main = async () => {
+  console.clear();
+
+  let opt = '';
+
+  do {
+    opt = await inquirerMenu();
+    console.log( { opt } );
+
+
+  } while ( opt !== '0' );
+
+};
+
+main();
