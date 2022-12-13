ca1412d6399835a4e7bf279ee283c92199f4b95c
angeles2710 <angcifolavioleta@gmail.com>
2022-12-13 09:39:55 +0100

    setup reparto tareas

---
+++ app/controllers/createTarea.js
@@ -0,0 +1,12 @@
+// CREATE: Tomás 
+// UPDATE: Iván
+// DELETE: Alejandro
+
+// Persistencia de datos: 
+// JSON: Iván 
+// SQL: Tomás
+// NOSQL: Alejandro
+
+// Inquirer: Alejandro
+// App: Tomás
+
