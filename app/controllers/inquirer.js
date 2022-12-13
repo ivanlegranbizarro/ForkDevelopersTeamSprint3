37b4cdf1b57323751b9fe65d0cc947b0122b3bc2
Alejandro Chazarreta <lcrender@gmail.com>
2022-12-13 10:34:07 +0100

    menu

---
+++ app/controllers/inquirer.js
@@ -0,0 +1,39 @@
+const inquirer = require("inquirer");
+
+const preguntas = [
+    {
+        type: 'list',
+        name: 'option',
+        message: 'Elige una opción',
+        choices: [
+            {
+                value: '1',
+                name: `${ '1.'.green } Crear tarea`
+            },
+            {
+                value: '2',
+                name: `${ '2.'.green } Listar tareas`
+            },
+            {
+                value: '3',
+                name: `${ '3.'.green } Listar tareas completadas`
+            },
+            {
+                value: '4',
+                name: `${ '4.'.yellow } Listar tareas pendientes`
+            },
+            {
+                value: '5',
+                name: `${ '5.'.green } Completar tarea(s)`
+            },
+            {
+                value: '6',
+                name: `${ '6.'.green } Borrar tarea`
+            },
+            {
+                value: '0',
+                name: `${ '0.'.red } Salir`
+            }
+        ]
+    }
+];
