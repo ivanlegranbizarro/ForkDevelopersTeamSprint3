66d7f40881fda00154668b51285a6e5abc367cda
Iván Legrán <68439095+ivanlegranbizarro@users.noreply.github.com>
2022-12-13 10:19:04 +0100

    setup persistencia local

---
+++ app/persistencia/JSON/guardarArchivo.js
@@ -0,0 +1,28 @@
+import * as fs from "fs";
+
+
+/* Declaring a constant variable called archivo and assigning it the value of the path to the data.json file. */
+const archivo = 'data.json';
+
+/**
+ * It takes a data parameter, and then writes that data to the file specified by the archivo variable
+ */
+const guardarArchivo = data => fs.writeFileSync(archivo, JSON.stringify(data));
+
+/**
+ * It reads the contents of a file and returns the data in the file as a JavaScript object
+ * @returns the data that is being read from the file.
+ */
+const leerArchivo = () => {
+    if (!fs.existsSync(archivo)) {
+        return null;
+    }
+    const info = fs.readFileSync(archivo, {encoding: 'utf-8'});
+    return JSON.parse(info);// TODO return data en lugar de info
+};
+
+
+export {
+    guardarArchivo,
+    leerArchivo
+};
