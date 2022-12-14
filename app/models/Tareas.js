8ebe5f8c25111bdbf0bd65c5ceea3ee0ca300240
Iván Legrán <ivanlegranbizarro@gmail.com>
2022-12-14 09:15:44 +0100

    Revert "metodo listado Array"
    This reverts commit ac8fedb1c4abb074f645195021f73299861b0bfe.

--- app/models/Tareas.js
+++ app/models/Tareas.js
@@ -4,27 +4,13 @@ import Tarea from "./Tarea.js";
 class Tareas {
     _listado = {};
 
-    constructor () {
+    constructor() {
         this._listado = {};
     }
-    borrarTarea ( id = '' ) {
-        if ( this._listado[ id ] ) {
-            delete this._listado[ id ];
+    borrarTarea(id = '') {
+        if (this._listado[id]) {
+            delete this._listado[id];
         }
-    }
-    get listadoArr () {
-        const listado = [];
-        Object.keys( this._listado ).forEach( key => {
-            const tarea = this._listado[ key ];
-            listado.push( tarea );
-        } );
-        return listado;
-    }
-
-    cargarTareasFromArray ( tareas = [] ) {
-        tareas.forEach( tarea => {
-            this._listado[ tarea.id ] = tarea;
-        } );
     }
 
 }
